34b9c5edbce604c2dff4dcf420ffbd4906ba209d
dejavu-v <dejavu@i.ua>
2016-07-06 21:40:24 +0300

    End Lesson 18

---
+++ src/Lesson18/ChatPanel.java
@@ -0,0 +1,14 @@
+package Lesson18;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by dejavu on 06.07.2016.
+ */
+public class ChatPanel extends JPanel {
+    public ChatPanel() {
+        this.setBackground(Color.green);
+
+    }
+}
---
+++ src/Lesson18/FriendsPanel.java
@@ -0,0 +1,13 @@
+package Lesson18;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by dejavu on 06.07.2016.
+ */
+public class FriendsPanel  extends JPanel {
+    public FriendsPanel() {
+        this.setBackground(Color.orange);
+    }
+}
