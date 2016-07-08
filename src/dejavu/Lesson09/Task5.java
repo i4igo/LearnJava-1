fe3b8adeb38386251d7f213fc7becff2b30750c9
dejavu <dejavu@mail.ua>
2016-07-08 19:06:04 +0300

    Finish Task2 HW by Lesson9

---
+++ src/dejavu/Lesson09/Task5.java
@@ -0,0 +1,31 @@
+package dejavu.Lesson09;
+
+import static dejavu.Lesson09.SelectTask.sc;
+
+/**
+ * Created by dejavu on 07.07.2016.
+ * Задание 1
+ * Анализ введеной строки и подсчет символов
+ */
+public class Task5 {
+    static {
+        new Task5();
+    }
+    private Task5() {
+        String inputText;
+        int toDo;
+
+        System.out.println("Введите текст:");
+        sc.nextLine();
+        inputText = sc.nextLine();
+
+        System.out.println("Выберите метод анализа\n" +
+                "1 - анализ с учетом регистра\n" +
+                "2 - анализ без учета регистра\n" +
+                "0 - выйти из программы");
+
+        toDo = sc.nextInt();
+
+        if(toDo == 0) return;
+    }
+}
