524ced4be3e9d8a18d944d0797782536ebaf7d5e
dejavu <dejavu@mail.ua>
2016-07-10 16:05:29 +0300

    Finish HW by Lesson9

--- src/dejavu/Lesson09/Task6.java
+++ src/dejavu/Lesson09/Task6.java
@@ -4,28 +4,14 @@ import static dejavu.Lesson09.SelectTask.sc;
 
 /**
  * Created by dejavu on 07.07.2016.
- * Задание 1
- * Анализ введеной строки и подсчет символов
+ * Задание 6
+ * Писатели книги, ArrayList<Sring[]>
  */
 public class Task6 {
     static {
         new Task6();
     }
     private Task6() {
-        String inputText;
-        int toDo;
 
-        System.out.println("Введите текст:");
-        sc.nextLine();
-        inputText = sc.nextLine();
-
-        System.out.println("Выберите метод анализа\n" +
-                "1 - анализ с учетом регистра\n" +
-                "2 - анализ без учета регистра\n" +
-                "0 - выйти из программы");
-
-        toDo = sc.nextInt();
-
-        if(toDo == 0) return;
     }
 }
--- src/dejavu/Lesson09/Task7.java
+++ src/dejavu/Lesson09/Task7.java
@@ -4,28 +4,74 @@ import static dejavu.Lesson09.SelectTask.sc;
 
 /**
  * Created by dejavu on 07.07.2016.
- * Задание 1
- * Анализ введеной строки и подсчет символов
+ * Задание 7
+ * Валидация Емейл
  */
 public class Task7 {
     static {
         new Task7();
     }
     private Task7() {
-        String inputText;
-        int toDo;
+        System.out.println("Программа проводит валидацию e-mail адреса");
+        inputMail();
 
-        System.out.println("Введите текст:");
-        sc.nextLine();
-        inputText = sc.nextLine();
+        getMenu();
+    }
 
-        System.out.println("Выберите метод анализа\n" +
-                "1 - анализ с учетом регистра\n" +
-                "2 - анализ без учета регистра\n" +
-                "0 - выйти из программы");
+    private void getMenu() {
+        System.out.println("Продолжить? Нажмите\n" +
+                "1 - Да,\n" +
+                "0 - Выйти из программы");
 
-        toDo = sc.nextInt();
+        if(sc.nextInt() == 1) inputMail();
+    }
 
-        if(toDo == 0) return;
+    private void inputMail() {
+        System.out.println("Введите email:");
+        String email = sc.next();
+
+        if( checkMail(email) ) System.out.println("Адрес " + email + " ПРОШЕЛ Валидацию. УРА!");
+        else inputMail();
+    }
+
+    private boolean checkMail(String email) {
+        char currentSymbol;
+
+        int lenghtMail = email.length();
+        int abes = email.indexOf("@");
+        int lastDot = email.lastIndexOf(".");
+
+        if(abes > lastDot) {
+            System.out.println("Нет точки в доменном адресе");
+            return false;
+        }
+
+        if( abes != email.lastIndexOf("@") ) {
+            System.out.println("Адрес содержит несколько символов @, что не допустимо! Введите адрес заново");
+            return false;
+        }
+
+        if( !email.contains(".") ) {
+            System.out.println("Адрес содержит ошибку в доменном имени - отсутствует точка! Введите адрес заново");
+            return false;
+        }
+
+        if( abes < 1
+                || (lenghtMail - abes - (lenghtMail - lastDot) ) < 2
+                || (lenghtMail - lastDot) < 4) {
+            System.out.println("Адрес является неполным. Введите адрес заново");
+            return false;
+        }
+
+        for (int i = 0; i < lenghtMail; i++){
+            currentSymbol = email.charAt(i);
+
+            if( !Character.isAlphabetic(currentSymbol) && currentSymbol != '@' && currentSymbol != '.') {
+                System.out.println("Адрес содержит содержит недопустимые символы. Введите адрес заново");
+                return false;
+            }
+        }
+
+        return true;
     }
 }
