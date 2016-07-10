a019b7212c892068fe1e2bfc78b7e07d87d65947
dejavu <dejavu@mail.ua>
2016-07-10 14:06:12 +0300

    Finish Task4 HW by Lesson9

--- src/dejavu/Lesson09/Task3.java
+++ src/dejavu/Lesson09/Task3.java
@@ -4,28 +4,112 @@ import static dejavu.Lesson09.SelectTask.sc;
 
 /**
  * Created by dejavu on 07.07.2016.
- * Задание 1
- * Анализ введеной строки и подсчет символов
+ * Задание 3
+ * Калькулятор
  */
 public class Task3 {
     static {
         new Task3();
     }
     private Task3() {
-        String inputText;
-        int toDo;
+        float firstNum;
+        float secondNum;
+        String action;
+        int resultType;
 
-        System.out.println("Введите текст:");
-        sc.nextLine();
-        inputText = sc.nextLine();
+        System.out.println("Введите первое число:");
+        firstNum = sc.nextFloat();
 
-        System.out.println("Выберите метод анализа\n" +
-                "1 - анализ с учетом регистра\n" +
-                "2 - анализ без учета регистра\n" +
-                "0 - выйти из программы");
+        System.out.println("Укажите действие");
+        action = getAction();
 
-        toDo = sc.nextInt();
+        System.out.println("Введите второе число:");
+        secondNum = sc.nextFloat();
 
-        if(toDo == 0) return;
+        System.out.println("Указанная операция будет проводится с введенными числами как с\n" +
+                "Целыми - нажмите 1\n" +
+                "Дробными - нажмите 2");
+        resultType = sc.nextInt();
+
+        if( resultType == 2 )
+            calculate( firstNum, secondNum, action);
+
+        else if( resultType == 1 )
+            calculate( (int) firstNum, (int) secondNum, action);
+
+        else System.out.println("Вы не выбрали тип чисел");
+
+        System.out.println("Продолжить? Нажмите\n" +
+                "1 - Да,\n" +
+                "0 - Выйти из программы");
+        if( sc.nextInt() == 1 ) new Task3();
+    }
+
+    private String getAction(){
+        String toDo;
+        String sings = "+-*/";
+
+        toDo = sc.next();
+
+        if(toDo.length() > 1 || toDo.length() == 0) {
+            System.out.println("Вы неправильно ввели знак действия. " +
+                    "Знак состоит из одного символа, Вы ввели " + toDo.length() + ". Введите еще раз");
+            getAction();
+        }
+
+        if( sings.indexOf(toDo) < 0 ) {
+
+            System.out.println("Вы неправильно ввели знак действия. Ваш знак " +
+                    toDo + " не является знаком математической операции. Введите еще раз");
+            getAction();
+        }
+
+        return "" + toDo;
+    }
+
+    private void calculate( float firstNum, float secondNum, String action ){
+        float result;
+
+        switch (action){
+            case "+":
+                result = firstNum + secondNum;
+                break;
+            case "-":
+                result = firstNum - secondNum;
+                break;
+            case "*":
+                result = firstNum * secondNum;
+                break;
+            case "/":
+                result = firstNum / secondNum;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+
+        System.out.println(firstNum + " " + action + " " + secondNum  + " = " + result);
+    }
+    private void calculate( int firstNum, int secondNum, String action ){
+        int result = 0;
+
+        switch (action){
+            case "+":
+                result = firstNum + secondNum;
+                break;
+            case "-":
+                result = firstNum - secondNum;
+                break;
+            case "*":
+                result = firstNum * secondNum;
+                break;
+            case "/":
+                result = firstNum / secondNum;
+                break;
+            default:
+                break;
+        }
+
+        System.out.println(firstNum + " " + action + " " + secondNum  + " = " + result);
     }
 }
--- src/dejavu/Lesson09/Task4.java
+++ src/dejavu/Lesson09/Task4.java
@@ -1,31 +1,100 @@
 package dejavu.Lesson09;
 
+import java.util.ArrayList;
+
 import static dejavu.Lesson09.SelectTask.sc;
 
 /**
  * Created by dejavu on 07.07.2016.
- * Задание 1
- * Анализ введеной строки и подсчет символов
+ * Задание 4
+ * База Данных сотрудников
  */
 public class Task4 {
+    ArrayList<String[]> db = new ArrayList<>();
+
     static {
         new Task4();
     }
     private Task4() {
-        String inputText;
-        int toDo;
+        db.add(new String[]{"Имя", "Фамилия", "Возраст", "Зарплата"});
 
-        System.out.println("Введите текст:");
-        sc.nextLine();
-        inputText = sc.nextLine();
+        System.out.print("База данных сотрудников. ");
+        getMenu();
+    }
 
-        System.out.println("Выберите метод анализа\n" +
-                "1 - анализ с учетом регистра\n" +
-                "2 - анализ без учета регистра\n" +
-                "0 - выйти из программы");
+    private void getMenu() {
+        int menuSelected;
 
-        toDo = sc.nextInt();
+        System.out.println("Введите:\n" +
+                "1 - добавить сотрудника\n" +
+                "2 - вивести список сотрудников\n" +
+                "0 - завершить программу");
 
-        if(toDo == 0) return;
+        menuSelected = sc.nextInt();
+
+        if( menuSelected == 1 ) addWorker();
+        else if ( menuSelected == 2) showWorkers();
+        else return;
+        getMenu();
+    }
+
+    private void addWorker() {
+        String firstName;
+        String lastName;
+        String age;
+        String salary;
+
+        System.out.println("Добавление информации по сотруднику.\n" +
+                "Введите фамилию сотрудника:");
+        lastName = sc.next();
+
+        System.out.println("Введите имя сотрудника:");
+        firstName = sc.next();
+
+        System.out.println("Введите возраст сотрудника:");
+        age = sc.next();
+
+        System.out.println("Введите зарплату сотрудника:");
+        salary = sc.next();
+
+        db.add(new String[]{lastName, firstName, age, salary});
+        System.out.println("Работник добавлен");
+    }
+
+    private void showWorkers() {
+        int maxLengths[] = getMaxLengths(db);
+        String outputText = "";
+
+        for (String[] worker: db){
+            int i = 0;
+            String row ="";
+
+            while( i < 4){
+                row += prepareString(worker[i], maxLengths[i]);
+                i++;
+            }
+
+            outputText += row + "\n";
+        }
+
+        System.out.println(outputText);
+    }
+
+    private int[] getMaxLengths(ArrayList<String[]> db) {
+        int maxLengths[] = {0,0,0,0};
+
+        for (String[] rows: db) {
+            int i = 0;
+            while ( i < 4 ) {
+                maxLengths[i] = (rows[i].length() > maxLengths[i]) ? rows[i].length() : maxLengths[i];
+                i++;
+            }
+        }
+
+        return maxLengths;
+    }
+
+    private String prepareString(String s, int maxLength) {
+        return String.format("%-" + maxLength + "s", s) + "\t";
     }
 }
